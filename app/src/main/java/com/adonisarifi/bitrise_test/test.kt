73a50c637ac05ba43577bf7cac90b0fd413d9a3c
Adonis Arifi <adonisarifi@gmail.com>
2018-06-11 11:05:38 +0200

    first commit

---
+++ app/src/main/java/com/adonisarifi/bitrise_test/test.kt
@@ -0,0 +1,10 @@
+package com.adonisarifi.bitrise_test
+
+
+/**
+ * Created by AArifi on 6/8/2018.
+ * Project: bitrisetest
+ * Github: https://github.com/aarifi
+ * Email "adonisarifi@gmail.com"
+ */
+
